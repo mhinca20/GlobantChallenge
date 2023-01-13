36b47e8ae26bc8b32086e9674d61d88a206d0192
Mateo <hincapiemateo@gmail.com>
2023-01-12 19:46:16 -0500

    Adding batch upload feature

--- app/main/controller/upload_csv.py
+++ app/main/controller/upload_csv.py
@@ -8,14 +8,17 @@ from ..service.csv_service import receive_data
 api = UploadDto.api
 
 #TODO add las ingested and batch size validation
-@api.route('/', methods=['GET', 'POST'])
+@api.route('/', methods=['POST'])
 @api.expect('file-type', 'File type to transfer')
 @api.expect('file', 'File to transfer')
 @api.expect('batch-size', 'batch size from 1 to 1000')
+@api.expect('next-row', 'next row')
 class Csv(Resource):
     def post(self):
         """"Receive data from CSV and save it into the database"""
         file_type = request.form.get('file-type')
         csv_file = request.files['file']
-        return receive_data(file_type,csv_file)
+        next_row = request.form.get('next-row')
+        next_row = 0 if not next_row else next_row
+        return receive_data(file_type, csv_file, int(request.form.get('batch-size')), int(next_row))
            
--- app/main/service/csv_service.py
+++ app/main/service/csv_service.py
@@ -5,6 +5,8 @@ import logging
 from app.main.model.employee import Employee
 from app.main.model.department import Department
 from app.main.model.job import Job
+from app.main.util.db_utils import save_changes
+
 
 
 from app.main import db
@@ -19,23 +21,31 @@ logging.basicConfig(filename=f'logs/data_upload_{datetime.now().strftime("%Y_%m_
 logger = logging.getLogger('Globant challenge')
 logger.setLevel(logging.ERROR)
 
-def receive_data(file_type, csv_file):
+def receive_data(file_type, csv_file, batch, next_row):
     response_object = None
     if file_type in csv_file.filename: 
-        csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in filter_csv(csv_reader):
-            if "employee" == file_type:
-                row = Employee(id=row[0], name=row[1], datetime=row[2], department_id=row[3], job_id=row[4])
-            elif "department" == file_type:
-                row = Department(id=row[0], department=row[1])
-            else:
-                row = Job(id=row[0], job=row[1])
-            save_changes(row)
-        response_object = {
-            'status': 'success',
-            'message': 'Data succesfully loaded.'
-        },201
+        if batch >= 1 and batch <= 1000 :
+            csv_file = TextIOWrapper(csv_file, encoding='utf-8')
+            csv_reader = list(csv.reader(csv_file, delimiter=','))
+            for row in filter_csv(csv_reader, next_row, batch):
+                if "employee" == file_type:
+                    row = Employee(id=row[0], name=row[1], datetime=row[2], department_id=row[3], job_id=row[4])
+                elif "department" == file_type:
+                    row = Department(id=row[0], department=row[1])
+                else:
+                    row = Job(id=row[0], job=row[1])
+                save_changes(row)
+            response_object = {
+                'status': 'success',
+                'message': 'Data succesfully loaded.',
+                'inserted_rows': str(batch),
+                'next_row': str(next_row + batch )
+            },201
+        else:
+            response_object = {
+            'status': 'fail',
+            'message': 'Batch size should be between 1 and 1000'
+        }      
     else:
         response_object = {
             'status': 'fail',
@@ -43,14 +53,10 @@ def receive_data(file_type, csv_file):
         }
     return response_object
 
-def filter_csv(csv_reader):
-    for row in csv_reader:
-        if '' in row:
-            logging.info("The following row is not going to be inserted, it contains empty values "+str(row))
+def filter_csv(csv_reader, next_row, batch):
+    for row in range(next_row, next_row + batch ):
+        if '' in csv_reader[row]:
+            logging.info("The following row is not going to be inserted, it contains empty values "+str(csv_reader[row]))
         else:
-            yield row
+            yield csv_reader[row]
     
-        
-def save_changes(data) -> None:
-    db.session.add(data)
-    db.session.commit()
